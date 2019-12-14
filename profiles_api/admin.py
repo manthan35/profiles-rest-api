961e8985d1d6558b53c58442e1b72ff2907bd7c7
manthan <manthan.moradiya2@gmail.com>
2019-12-14 15:20:30 +0530

    added Hello APTView

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from profiles_api import models
 
 # Register your models here.
-admin.site.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['email','name']
+    
+
+admin.site.register(models.UserProfile,UserProfileAdmin)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test Api View"""
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses Http method as a function(get,post,patch,push,delete)',
+            'Is similar to a traditional django view',
+            'Gives you the most control over your application logic',
+            'It mapped manually to URLs',
+        ]
+        return Response({'message':'Hello!!', 'an_apiview':an_apiview})
